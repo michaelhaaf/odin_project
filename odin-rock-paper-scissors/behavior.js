b890edeeb8bf1a57c5d660ebfc98840a1f984d36
michaelhaaf <michael.haaf@gmail.com>
2022-12-05 12:02:23 -0500

    complete 5 round rock paper scissors. It works though it's kind of a piece of shit

--- odin-rock-paper-scissors/behavior.js
+++ odin-rock-paper-scissors/behavior.js
@@ -1,5 +1,5 @@
 function capitalize(string) {
-  let result = string.slice(0,1).toUpperCase() + string.slice(1,).toLowerCase()
+  let result = string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
   return result;
 }
 
@@ -13,23 +13,45 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   if (playerSelection === computerSelection) {
-    result =
-      `Tie! ${capitalize(playerSelection)} and ${capitalize(computerSelection)} the same, no conclusive winner.`;
+    result = `Tie! ${capitalize(playerSelection)} and ${capitalize(
+      computerSelection
+    )} the same, no conclusive winner.`;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    result = `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+    result = `You win! ${capitalize(playerSelection)} beats ${capitalize(
+      computerSelection
+    )}`;
   } else {
-    result = `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+    result = `You lose! ${capitalize(computerSelection)} beats ${capitalize(
+      playerSelection
+    )}`;
   }
 
   return result;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(`Player selection: ${playerSelection}`)
-console.log(`Computer selection: ${computerSelection}`)
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    console.log(`Player selection: ${playerSelection}`);
+    console.log(`Computer selection: ${computerSelection}`);
+    result = playRound(playerSelection, computerSelection);
+    if (result.includes("You win")) {
+      playerScore++;
+    } else if (result.includes("You lose")) {
+      computerScore++;
+    }
+    console.log(result)
+    console.log(`Player score: ${playerScore}`)
+    console.log(`Computer score: ${computerScore}`)
+  }
+  console.log(`Game over!`)
+}
+
+game();
