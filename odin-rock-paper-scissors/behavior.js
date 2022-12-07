79bdd62d5ea1aec1e040fbc425c6e8ec864d9796
michaelhaaf <michael.haaf@gmail.com>
2022-12-06 19:32:44 -0500

    Refactor buttonClick function to separate and explicit name.
    Also finish score >=5 win conditions. Some decisions inspired by source
    from top solution on OdinProject.
    https://lookingcoolonavespa.github.io/rock-paper-scissors/

--- odin-rock-paper-scissors/behavior.js
+++ odin-rock-paper-scissors/behavior.js
@@ -1,23 +1,20 @@
-function capitalize(string) {
-  let result = string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
-  return result;
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+
+let buttons = Array.from(document.querySelectorAll("button"));
+buttons.forEach((button) =>
+  button.addEventListener("click", buttonClickBeginRound)
+);
+
+function buttonClickBeginRound(e) {
+  playerSelection = this.textContent.toLowerCase();
+  computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection)
 }
 
-function getComputerChoice() {
-  let choices = ["rock", "paper", "scissors"];
-  let choice = Math.floor(Math.random() * choices.length);
-  return choices[choice];
-}
-
-
-function playRound(e) {
-  const pScore = document.querySelector("#pScore");
-  const cScore = document.querySelector("#cScore");
-
-  const playerSelection = e.target.textContent.toLowerCase();
-  const computerSelection = getComputerChoice();
-  document.querySelector("#cChoice").textContent = capitalize(computerSelection);
-
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     result = `Tie! ${capitalize(playerSelection)} and ${capitalize(
       computerSelection
@@ -27,20 +24,47 @@ function playRound(e) {
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
+    playerScore++;
     result = `You win! ${capitalize(playerSelection)} beats ${capitalize(
       computerSelection
     )}`;
-    pScore.textContent = 1 + Number.parseInt(pScore.textContent);
   } else {
+    computerScore++;
     result = `You lose! ${capitalize(computerSelection)} beats ${capitalize(
       playerSelection
     )}`;
-    cScore.textContent = 1 + Number.parseInt(pScore.textContent);
   }
-
-  document.querySelector("#result").textContent = result;
+  updateScore();
+  displayResults(result);
 }
 
+function updateScore() {
+  let playerScoreBox = document.querySelector("#playerScoreBox");
+  let computerScoreBox = document.querySelector("#computerScoreBox");
+  playerScoreBox.textContent = playerScore;
+  computerScoreBox.textContent = computerScore;
+}
 
-const buttons = Array.from(document.querySelectorAll("button"));
-buttons.forEach((button) => button.addEventListener("click", playRound));
+function displayResults(str) {
+  let gameResult = document.querySelector("#gameResult");
+  let matchResult = document.querySelector("#matchResult");
+
+  gameResult.textContent = str;
+  if (playerScore >= 5) {
+    matchResult.textContent = "You win!";
+  } else if (computerScore >= 5) {
+    matchResult.textContent = "You lose!";
+  }
+}
+
+// Helper Functions //
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let choice = Math.floor(Math.random() * choices.length);
+  return choices[choice];
+}
