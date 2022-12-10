4967b1c39e854486de31209e7c084b70d1df59d3
michaelhaaf <michael.haaf@gmail.com>
2022-12-10 03:08:40 -0500

    Complete draft etch-a-sketch. Opacity not quite yet implemented.
    There were two iterations merged into this commit by mistake.
    1st. working implementation of bare minimum requiremnets
    2nd. improvemnets heavily inspired by  on top voted example
    (styling ideas and better practises in .js)

---
+++ odin-etch-a-sketch/app.js
@@ -0,0 +1,86 @@
+const sketchBoard = document.querySelector("#sketchBoard");
+const selectedColor = document.querySelector("#selectedColor");
+const colorModeBtn = document.querySelector("#colorModeBtn");
+const randomModeBtn = document.querySelector("#randomModeBtn");
+const eraserModeBtn = document.querySelector("#eraserModeBtn");
+const resetBoardBtn = document.querySelector("#resetBoardBtn");
+const sizeRange = document.querySelector("#sizeRange");
+const sizeValue = document.querySelector("#sizeValue");
+const opacityRange = document.querySelector("#opacityRange");
+const opacityValue = document.querySelector("#opacityValue");
+
+const DEFAULT_COLOR = "#000000";
+const DEFAULT_ERASER = "#FFFFFF";
+
+// JS Pattern: events are "short-lived"; to combine events, decide on state
+let mouseDown = false;
+let activeModeBtn = colorModeBtn;
+
+function updatePixelColor(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+
+  // this.style.backgroundColor = selectedColor.style.backgroundColor;
+  let newColor = DEFAULT_COLOR;
+  if (activeModeBtn === randomModeBtn) {
+    const randomColor = Math.floor(Math.random() * 256 ** 3).toString(16);
+    newColor = `#${randomColor}`;
+  } else if (activeModeBtn === colorModeBtn) {
+    newColor = selectedColor.value;
+  } else if (activeModeBtn === eraserModeBtn) {
+    newColor = DEFAULT_ERASER;
+  } 
+
+  // newColor += opacityRange.value.toString(16);
+  selectedColor.value = newColor;
+  this.style.backgroundColor = newColor;
+
+}
+
+function resetBoard() {
+  sketchBoard.innerHTML = "";
+  setupSketchBoard(sizeRange.value);
+}
+
+function setupSketchBoard(size) {
+  sketchBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  sketchBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.addEventListener("mouseover", updatePixelColor);
+    pixel.addEventListener("mousedown", updatePixelColor);
+    sketchBoard.appendChild(pixel);
+  }
+}
+
+function updateOpacity(e) {
+  const percentage = (100 * this.value / 255).toFixed(2)
+  opacityValue.textContent = `${percentage}%`;
+}
+
+function updateSize(e) {
+  sizeValue.textContent = `${this.value} x ${this.value}`;
+}
+
+function selectMode(e) {
+  activeModeBtn.classList.remove("active");
+  this.classList.add("active");
+  activeModeBtn = this;
+}
+
+sketchBoard.addEventListener("mousedown", () => (mouseDown = true));
+sketchBoard.addEventListener("mouseup", () => (mouseDown = false));
+sizeRange.addEventListener("input", updateSize);
+opacityRange.addEventListener("input", updateOpacity);
+
+colorModeBtn.addEventListener("click", selectMode);
+randomModeBtn.addEventListener("click", selectMode);
+eraserModeBtn.addEventListener("click", selectMode);
+resetBoardBtn.addEventListener("click", resetBoard);
+
+
+window.onload = () => {
+  setupSketchBoard(sizeRange.value);
+  selectedColor.value = DEFAULT_COLOR;
+};
