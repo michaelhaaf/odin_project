ad1f249d641deadcd78ea9784ed1c65ea45902ae
michaelhaaf <michael.haaf@gmail.com>
2022-12-30 14:25:55 -0500

    start odin project calculator

---
+++ odin-calculator/app.js
@@ -0,0 +1,28 @@
+const DEFAULT_COLOR = "#000000";
+const DEFAULT_ERASER = "#FFFFFF";
+const DEFAULT_OPACITY = 1.0;
+const DEFAULT_SIZE = 16;
+
+const calculator = document.querySelector("#calculator");
+
+// calculator.addEventListener("mousedown", () => (mouseDown = true));
+
+/*
+ * Window load event and methods
+ */
+function setLabelDefaults() {
+
+  // sizeRange.value = DEFAULT_SIZE;
+  // sizeLabel.textContent = `${DEFAULT_SIZE} x ${DEFAULT_SIZE}`;
+  // opacityRange.value = DEFAULT_OPACITY;
+  // opacityLabel.textContent = DEFAULT_OPACITY;
+  //
+}
+
+window.onload = () => {
+
+  // setLabelDefaults();
+  // setupSketchBoard(sizeRange.value);
+  // updateSelectedColor(DEFAULT_COLOR);
+  //
+};
