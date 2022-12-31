954bf4829a8c2780ebdfd691c6f38de833646732
michaelhaaf <michael.haaf@gmail.com>
2022-12-31 13:13:23 -0500

    Implement basic operation and number button functionality
    also renamed some things

--- odin-calculator/app.js
+++ odin-calculator/app.js
@@ -1,53 +1,78 @@
-const CONSTANT = "";
+/*
+ * Functions
+ */
 
-const currentDisplayDiv = document.querySelector(".current-display");
-const previousDisplayDiv = document.querySelector(".previous-display");
+const updateDisplay = function (displayDiv, output) {
+  displayDiv.textContent = output;
+};
 
-let currentDisplay = 0;
-let previousDisplay = "";
+const add = function (x, y) {
+  return x + y;
+};
 
-// calculator.addEventListener("mousedown", () => (mouseDown = true));
+const subtract = function (x, y) {
+  return x - y;
+};
+
+const divide = function (x, y) {
+  return x / y;
+};
+
+const multiply = function (x, y) {
+  return x * y;
+};
+
+const operate = function (operation, x, y) {
+  return operation(x, y);
+};
+
+/*
+ * Setup
+ */
+
+const currentDisplayDiv = document.querySelector(".display-current");
+const previousDisplayDiv = document.querySelector(".display-previous");
+const operationButtons = Array.from(document.querySelectorAll(".operation"));
+const numberButtons = Array.from(document.querySelectorAll(".number"));
+
+const operations = new Map();
+operations.set(document.querySelector("#add-button"), add);
+operations.set(document.querySelector("#subtract-button"), subtract);
+operations.set(document.querySelector("#divide-button"), divide);
+operations.set(document.querySelector("#multiply-button"), multiply);
+
+let queuedOperation = add; 
+let currentOperand = "";
+let queuedOperand = "";
+
+operationButtons.map((button) => {
+  button.addEventListener("click", () => {
+    let result = operate(queuedOperation, +queuedOperand, +currentOperand);
+    updateDisplay(currentDisplayDiv, result)
+
+    queuedOperation = operations.get(button);
+    queuedOperand = result;
+    currentOperand = "";
+
+    updateDisplay(
+      previousDisplayDiv,
+      queuedOperand + " " + button.textContent
+    );
+  });
+});
+
+numberButtons.map((button) => {
+  button.addEventListener("click", () => {
+    currentOperand += button.textContent;
+    updateDisplay(currentDisplayDiv, currentOperand);
+  });
+});
 
 /*
  * Window load event and methods
  */
-function setLabelDefaults() {
-
-  // sizeRange.value = DEFAULT_SIZE;
-  // sizeLabel.textContent = `${DEFAULT_SIZE} x ${DEFAULT_SIZE}`;
-  // opacityRange.value = DEFAULT_OPACITY;
-  // opacityLabel.textContent = DEFAULT_OPACITY;
-  
-}
 
 window.onload = () => {
-
-  // setLabelDefaults();
-  // setupSketchBoard(sizeRange.value);
-  // updateSelectedColor(DEFAULT_COLOR);
-  //
+  updateDisplay(currentDisplayDiv, "0");
+  updateDisplay(previousDisplayDiv, "");
 };
-
-/* 
- * Functions
- */
-
-const add = function(x, y) {
-  return x+y;
-}
-
-const subtract = function(x, y) {
-  return x-y;
-}
-
-const divide = function(x, y) {
-  return x\y;
-}
-
-const multiply = function(x, y) {
-  return x*y;
-}
-
-const operate = function(operation, x, y) {
-  return operation(x, y);
-}
